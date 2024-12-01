e0e8fe862b43050046f3e462ea5f48bc4d782d1a
estefanemnz <estefanemenez@gmail.com>
2024-12-01 16:53:39 -0300

    Sidebar adicionada

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
 import './global.css';
+import styles from '@/App.module.css'
+
 
 import {Header} from './Components/Header/Header';
-
-
+import {Sidebar} from  './Components/Sidebar/Sidebar'
 function App() {
 
-  const [count, setCount] = useState(0)
-
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+            <p>Aqui ficam os posts.</p>
+        </main>
+      </div>
+    </div>  
   )
 }
 
---
+++ ignite-feed/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react';
+
+export function Sidebar(){
+
+    return(
+        <aside className={styles.sidebar}>
+            <img  className={styles.cover} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOsmc0P_lR1DnfV-O4fChPOr2sdJucOnVrUg&s"></img>
+            
+            <div className={styles.profile}>
+                <img src="https://conteudo.imguol.com.br/c/noticias/2013/09/02/nas-redes-sociais-gato-com-cara-de-triste-atrai-milhares-de-seguidores-1378162490423_300x300.jpg.webp"/>
+                <strong>Estefane Andrade</strong>
+                <span>Front End Developer</span>
+            </div>
+
+            <footer>
+                <a href='#'>
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>      
+            </footer>
+        </aside>
+    )
+
+}
